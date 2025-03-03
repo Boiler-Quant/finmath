a2a239e7e233a1f6543f6583fd531cdac0eff58e
shashank524 <saishashank524@gmail.com>
2025-03-02 20:34:37 -0500

    new tests

--- src/python_bindings.cpp
+++ src/python_bindings.cpp
@@ -1,5 +1,5 @@
 #include <pybind11/pybind11.h>
-#include <pybind11/stl.h>  // Automatic conversion between Python lists and std::vector
+#include <pybind11/stl.h> // Automatic conversion between Python lists and std::vector
 
 #include "finmath/InterestAndAnnuities/compound_interest.h"
 #include "finmath/OptionPricing/black_scholes.h"
@@ -10,34 +10,37 @@
 
 namespace py = pybind11;
 
-PYBIND11_MODULE(finmath, m) {
-    m.doc() = "Financial Math Library";
+PYBIND11_MODULE(finmath, m)
+{
+      m.doc() = "Financial Math Library";
 
-    // Expose the OptionType enum class
-    py::enum_<OptionType>(m, "OptionType")
-        .value("CALL", OptionType::CALL)
-        .value("PUT", OptionType::PUT)
-        .export_values();
+      // Expose the OptionType enum class
+      py::enum_<OptionType>(m, "OptionType")
+          .value("CALL", OptionType::CALL)
+          .value("PUT", OptionType::PUT)
+          .export_values();
 
-    // Bind compound interest function
-    m.def("compound_interest", &compound_interest, "Calculate compound interest",
-          py::arg("principal"), py::arg("rate"), py::arg("time"), py::arg("frequency"));
+      // Bind compound interest function
+      m.def("compound_interest", &compound_interest, "Calculate compound interest",
+            py::arg("principal"), py::arg("rate"), py::arg("time"), py::arg("frequency"));
 
-    // Bind Black-Scholes function
-    m.def("black_scholes", &black_scholes, "Black Scholes Option Pricing",
-          py::arg("type"), py::arg("strike"), py::arg("price"), py::arg("time"), py::arg("rate"), py::arg("volatility"));
+      // Bind Black-Scholes function
+      m.def("black_scholes", &black_scholes, "Black Scholes Option Pricing",
+            py::arg("type"), py::arg("strike"), py::arg("price"), py::arg("time"), py::arg("rate"), py::arg("volatility"));
 
-    // Bind binomial option pricing function
-    m.def("binomial_option_pricing", &binomial_option_pricing, "Binomial Option Pricing",
-          py::arg("type"), py::arg("S0"), py::arg("K"), py::arg("T"), py::arg("r"), py::arg("sigma"), py::arg("N"));
+      // Bind binomial option pricing function
+      m.def("binomial_option_pricing", &binomial_option_pricing, "Binomial Option Pricing",
+            py::arg("type"), py::arg("S0"), py::arg("K"), py::arg("T"), py::arg("r"), py::arg("sigma"), py::arg("N"));
 
-    // Bind rolling volatility
-    m.def("rolling_volatility", &rolling_volatility, "Rolling Volatility",
-          py::arg("prices"), py::arg("window_size"));
+      // Bind rolling volatility
+      m.def("rolling_volatility", &rolling_volatility, "Rolling Volatility",
+            py::arg("prices"), py::arg("window_size"));
 
-    m.def("simple_moving_average", &simple_moving_average, "Simple Moving Average",
-          py::arg("prices"), py::arg("window_size"));
+      // Bind simple moving average
+      m.def("simple_moving_average", &simple_moving_average, "Simple Moving Average",
+            py::arg("prices"), py::arg("window_size"));
 
-    m.def("rsi", &compute_rsi, "Relative Strength Index(RSI)",
-          py::arg("prices"), py::arg("window_size"));
+      // Bind RSI
+      m.def("rsi", &compute_rsi, "Relative Strength Index(RSI)",
+            py::arg("prices"), py::arg("window_size"));
 }
